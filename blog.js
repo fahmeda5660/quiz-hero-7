620f493ab0b82a22a2bdf1f2c8a1968a59204158
fahmeda5660 <fariha.208860@gmail.com>
2023-07-01 23:20:01 +0600

    first commit

---
+++ blog.js
@@ -0,0 +1,9 @@
+document.getElementById('blog-btn').addEventListener('click',function(){
+    // console.log('okay');
+    window.location.href = 'blog.html';
+});
+
+document.getElementById('blog-btn-1').addEventListener('click',function(){
+    // console.log('okay');
+    window.location.href = 'index.html';
+});
---
+++ index-blog.js
@@ -0,0 +1,4 @@
+document.getElementById('blog-btn-1').addEventListener('click',function(){
+    // console.log('okay');
+    window.location.href = 'index.html';
+});
